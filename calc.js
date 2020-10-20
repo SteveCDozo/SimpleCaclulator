8160733780eb79670cd2d9674d9fac2189935268
Steve <steve.c.cardozo@gmail.com>
2020-10-20 15:25:11 -0400

    started on processing basic expressions

--- calc.js
+++ calc.js
@@ -6,23 +6,31 @@ const opButtons    = document.querySelectorAll('.op-btn');
 const cButton      = document.querySelector('#c-btn');
 const acButton     = document.querySelector('#ac-btn');
 
+var num1 = "", num2 = "", op = "";
+
+// for debugging
+const debugOp   = document.querySelector('#debug-op');
+const debugNum1 = document.querySelector('#debug-num1');
+const debugNum2 = document.querySelector('#debug-num2');
+
 
 // send text to the display when buttons are clicked
 numButtons.forEach(button => {
     button.addEventListener('mousedown', () => {
-        inputLine.innerText += button.innerText;
+        processNum(button.innerText);
     });
 });
 
 opButtons.forEach(button => {
     button.addEventListener('mousedown', () => {
-        inputLine.innerText += button.innerText;
+        processOp(button.innerText);
     });
 });
 
 // clear button will clear the current line for now
 cButton.addEventListener('mousedown', () => {
     inputLine.innerText = "";
+    updateDebugInfo();
 });
 
 // all clear button clears everything on the display
@@ -56,4 +64,58 @@ function clearDisplay() {
     displayLines.forEach((line) => {
         line.innerText = "";
     });
+    updateDebugInfo();
+}
+
+function processNum(digit) {
+    write(digit);
+    // if an operation is already registered, then this digit is part of the second number
+    // otherwise it is part of the first number
+    if (op == "")
+        num1 += digit;
+    else
+        num2 += digit;
+    updateDebugInfo();
+}
+
+function processOp(operation) {
+    if (operation == "=") {
+        processExpression();
+    } else {        
+        // if there's already a second number, then first process the current expresion before overwriting op
+        if (num2 != "") {
+            num1 = processExpression();
+            write(num1);
+        }
+        op = operation;  
+        write(op);               
+    }
+    updateDebugInfo();
+}
+
+function processExpression() {
+    const ans = "ANS";
+    write("="+ans);
+    advanceLines();
+    op = "";
+    num1 = "";
+    num2 = "";    
+    return ans;
+}
+
+function write(text) {
+    inputLine.innerText += text;
+}
+
+function advanceLines() {
+    for (let x = 1; x < 4; ++x) {
+        document.querySelector('#line'+x).innerText = document.querySelector('#line'+(x+1)).innerText;
+    }
+    inputLine.innerText = "";
+}
+
+function updateDebugInfo() {
+    debugOp.innerText   = op;
+    debugNum1.innerText = num1;
+    debugNum2.innerText = num2;
 }
