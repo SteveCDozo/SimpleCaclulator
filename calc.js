d0b2cebd31b1e35eb95c39bc306ea42f821c056d
HyeonChan Lee <hyeonchanlee@gmail.com>
2020-10-20 23:12:15 -0400

    Updated num op algorithm

--- calc.js
+++ calc.js
@@ -6,7 +6,7 @@ const opButtons    = document.querySelectorAll('.op-btn');
 const cButton      = document.querySelector('#c-btn');
 const acButton     = document.querySelector('#ac-btn');
 
-var num1 = "", num2 = "", op = "";
+var num1 = "", num2 = "", op = "", ans = "";
 
 // for debugging
 const debugOp   = document.querySelector('#debug-op');
@@ -30,11 +30,14 @@ opButtons.forEach(button => {
 // clear button will clear the current line for now
 cButton.addEventListener('mousedown', () => {
     inputLine.innerText = "";
+    clearNumOp();
     updateDebugInfo();
 });
 
 // all clear button clears everything on the display
 acButton.addEventListener('mousedown', () => {
+    clearNumOp();
+    ans = ""; // Clear memory of previous answer as well 
     clearDisplay();
 });
 
@@ -80,16 +83,50 @@ function processNum(digit) {
 
 function processOp(operation) {
     if (operation == "=") {
-        processExpression();
-    } else {        
-        // if there's already a second number, then first process the current expresion before overwriting op
-        if (num2 != "") {
-            num1 = processExpression();
+        // Ignore "=" if expression is not complete
+        if (num1 && op && num2) {
+            ans = processExpression();
+        }
+    } else if (operation == "+/-") {
+        if (num2) {
+            erase(num2);
+            num2 = -1 * parseInt(num2);
+            num2 = num2.toString();
+            write(num2);
+        } else if (num1 && !op) {
+            erase(num1);
+            num1 = -1 * parseInt(num1);
+            num1 = num1.toString();
             write(num1);
         }
-        op = operation;  
-        write(op);               
+    } else {
+        // If second number exists, process expression, and make num1 the answer
+        if (num2) {
+            ans = processExpression();
+            // Below two lines can be deleted, as processing expression will clear num1 
+            // and lead into the next if statement
+            num1 = ans;
+            write(num1);
+        } 
+        // If first number doesn't exist, bring the previous answer as num1
+        if (!num1) {
+            num1 = ans;
+            write(num1);
+        }
+        op = operation;
+        write(op);
     }
+    // if (operation == "=") {
+    //     processExpression();
+    // } else {        
+    //     // if there's already a second number, then first process the current expresion before overwriting op
+    //     if (num2 != "") {
+    //         num1 = processExpression();
+    //         write(num1);
+    //     }
+    //     op = operation;  
+    //     write(op);               
+    // }
     updateDebugInfo();
 }
 
@@ -97,14 +134,17 @@ function processExpression() {
     const ans = "ANS";
     write("="+ans);
     advanceLines();
-    op = "";
-    num1 = "";
-    num2 = "";    
+    clearNumOp(); // Clear num and op upon processing expression, and get ready for next
     return ans;
 }
 
 function write(text) {
     inputLine.innerText += text;
+}
+
+// Erase last input from end of input line
+function erase(text) {
+    inputLine.innerText = inputLine.innerText.slice(0, inputLine.innerText.length - text.length);
 }
 
 function advanceLines() {
@@ -118,4 +158,11 @@ function updateDebugInfo() {
     debugOp.innerText   = op;
     debugNum1.innerText = num1;
     debugNum2.innerText = num2;
+}
+
+// Clear num1, num2, and op
+function clearNumOp() {
+    op = "";
+    num1 = "";
+    num2 = "";
 }
